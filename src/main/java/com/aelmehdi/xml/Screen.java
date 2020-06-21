754f3b09f17cc9114857603918e64b877c81d33b
AElMehdi <assirelmehdi@gmail.com>
2020-06-21 23:44:23 +0200

    Inject literal value using p namespace

--- src/main/java/com/aelmehdi/xml/Screen.java
+++ src/main/java/com/aelmehdi/xml/Screen.java
@@ -1,9 +1,13 @@
 package com.aelmehdi.xml;
 
 public class Screen {
-    private static final String brand = "Samsoul";
+    private String brand;
 
     public void brand() {
         System.out.println("Screen brand name:" + brand);
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
     }
 }
